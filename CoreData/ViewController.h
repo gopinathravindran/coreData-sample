ff1c4d1aecb4fb8d2331f363efd6360b1efbff75
Orbgen <orbgen@orbgen-045.orbgentech.com>
2015-11-28 11:30:53 +0530

    Initial Commit

---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by Orbgen on 28/11/15.
+//  Copyright (c) 2015 Orbgen Technologies pvt. ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreData
+//
+//  Created by Orbgen on 28/11/15.
+//  Copyright (c) 2015 Orbgen Technologies pvt. ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
